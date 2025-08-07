ded31d35f83d741b8dd0096fed9a2cdd3e706895
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-08-07 04:46:14 +0200

    [ADD] src/discord.cjs

---
+++ src/discord.cjs
@@ -0,0 +1,23 @@
+module.exports = async (discordClient, twitchClient) => {
+
+  discordClient.on('ready', () => {
+    console.log(`Logged in as ${discordClient.user.tag}!`);
+    console.log(`Connected to Twitch channel: ${process.env.TWITCH_CHANNEL}`);
+
+    twitchClient.connect();
+  });
+
+  discordClient.on('messageCreate', async message => {
+    if (message.channel.id === process.env.DISCORD_CHANNEL_ID && !message.author.bot) {
+      const twitchMessage = `${message.author.tag} (Discord): ${message.content}`;
+
+      twitchClient.say(process.env.TWITCH_CHANNEL, twitchMessage)
+        .then(() => {
+          console.log(`Relayed message from Discord to Twitch: "${twitchMessage}"`);
+        })
+        .catch(error => {
+          console.error('Error relaying message to Twitch:', error);
+        });
+    }
+  });
+}
