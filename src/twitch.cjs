08d1bb70f44ace8e63947195ed36c08fc05d1d10
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-08-08 14:40:22 +0200

    [UPDATE] twitch.cjs
    [ADD] on 'subscribe' event

--- src/twitch.cjs
+++ src/twitch.cjs
@@ -1,5 +1,5 @@
 module.exports = async (discordClient, twitchClient) => {
-  twitchClient.on('message', (_, tags, message, self) => {
+  twitchClient.on('message', (tags, message, self) => {
     if (self) return;
 
     const discordChannel = discordClient.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
@@ -8,4 +8,25 @@ module.exports = async (discordClient, twitchClient) => {
       discordChannel.send(`**${tags['display-name']}** (Twitch): ${message}`);
     }
   });
+
+  twitchClient.on('subscription', (username, tags) => {
+    const months = tags['msg-param-cumulative-months'];
+
+    const discordChannel = discordClient.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
+
+    if (discordChannel) {
+      let subMessage = `🎉 A new subscriber! **${username}** just subscribed to the channel!`;
+      if (months) {
+        subMessage = `🎉 A new sub from **${username}**! This is their ${months} month in a row!`;
+      }
+
+      discordChannel.send(subMessage)
+        .then(() => {
+          console.log(`Relayed subscription notification to Discord: "${subMessage}"`);
+        })
+        .catch(error => {
+          console.error('Error relaying subscription to Discord:', error);
+        });
+    }
+  })
 }
