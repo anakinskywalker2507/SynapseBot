663d6549aa1776c80baf706a418e6a792afec49f
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-08-12 01:52:43 +0200

    [UPDATE & RENAME] appcmds/help.mjs => cmds/help.mjs
    [ADD] async msg() to command

--- src/cmds/help.mjs
+++ src/cmds/help.mjs
@@ -86,6 +86,78 @@ export const command = {
     }
 
     await interaction.reply({ embeds: [Embed] });
+  },
+
+  async msg(bot, message, args) {
+    let cmd;
+    if (args[0]) {
+      cmd = bot.commands.get(args[0]);
+      if (!cmd)
+        return message.reply(`Error: \`${args[0]}\` is not a command.`);
+    }
+
+    let Embed;
+    if (!cmd) {
+      let categories = [];
+      bot.commands.forEach(command => {
+        if (!categories.includes(command.category)) categories.push(command.category);
+      })
+
+      Embed = new Discord.EmbedBuilder()
+        .setColor(bot.color)
+        .setTitle(`Commands`)
+        .setAuthor({
+          name: message.member.user.username,
+          iconURL: message.member.user.displayAvatarURL({ dynmaic: true })
+        })
+        .setThumbnail(bot.user.displayAvatarURL({ dynamic: true }))
+        .setDescription(`Prefix: \`${bot.prefix}\`\nAvailable commands: \`${bot.commands.size}\`\nAvailable categories: \`${categories.length}\``)
+        .setTimestamp()
+        .setFooter({
+          text: "/help",
+          iconURL: bot.user.displayAvatarURL({ dynamic: true })
+        });
+
+      categories.sort().forEach(async cat => {
+        let commands = bot.commands.filter(cmd => cmd.category === cat);
+        Embed.addFields({ name: `${cat}`, value: `${commands.map(cmd => `\`${cmd.name}\`: \*\*${cmd.description}\*\*`).join("\n")}`, inline: false });
+      });
+    } else {
+      Embed = new Discord.EmbedBuilder()
+        .setColor(bot.color)
+        .setTitle(`\`${cmd.name}\` command`)
+        .setAuthor({
+          name: message.member.user.username,
+          iconURL: message.member.user.displayAvatarURL({ dynamic: true })
+        })
+        .setThumbnail(bot.user.displayAvatarURL({ dynamic: true }))
+        .setTimestamp()
+        .setFooter({
+          text: `/help ${cmd.name}`,
+          iconURL: bot.user.displayAvatarURL({ dynamic: true })
+        });
+
+      let context = cmd.context;
+      if (typeof context !== "string") {
+        if (context === 0) context = "Guild"
+        else if (context === 1) context = "DM"
+        else if (context === 2) context = "PrivateDM"
+        else context = "Error: Context"
+      }
+
+      Embed.addFields({ name: `Name`, value: `\`${cmd.name}\``, inline: true });
+      Embed.addFields({ name: `Description`, value: `\`${cmd.description}\``, inline: false });
+      Embed.addFields({
+        name: `Requied Permission`,
+        value: `\`${typeof cmd.permission !== "bigint" ?
+          cmd.permission : new Discord.PermissionsBitField(cmd.permission).toArray()}\``,
+        inline: true
+      });
+      Embed.addFields({ name: `Context`, value: `\`${context}\``, inline: true });
+      Embed.addFields({ name: `Category`, value: `\`${cmd.category}\``, inline: true });
+    }
+
+    await message.reply({ embeds: [Embed] });
   }
 }
 
