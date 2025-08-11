6905a4e13c3d4852a19beeb70c1f483b8e9100cf
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-08-12 01:54:23 +0200

    [FIX] loadCommands.cjs
    [EDIT] appcmds/ => cmds/

--- src/loaders/loadCommands.cjs
+++ src/loaders/loadCommands.cjs
@@ -3,9 +3,9 @@ const fs = require("fs")
 module.exports = async discordClient => {
 
   console.log("[⏳]\x1b[33mCommands Loading...\x1b[0m");
-  fs.readdirSync("./src/appcmds/").filter(f => f.endsWith(".mjs")).forEach(async file => {
+  fs.readdirSync("./src/cmds/").filter(f => f.endsWith(".mjs")).forEach(async file => {
 
-    let command = require(`../appcmds/${file}`).command;
+    let command = require(`../cmds/${file}`).command;
     if (!command.name || typeof command.name !== "string") throw new TypeError(`Command ${file.slice(0, file.length - 4)} doesn't have a name !`)
     discordClient.commands.set(command.name, command)
     console.log(`   [✅]\x1b[32m${file.slice(0, file.length - 4)}\x1b[0m`);
