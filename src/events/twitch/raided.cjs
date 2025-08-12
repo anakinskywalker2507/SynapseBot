0e47ccbc293d2e3901cd44d0267d3f717246db6e
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-08-12 22:18:41 +0200

    [FIX] raided.cjs
    [ADD] _twitchClient parameter

--- src/events/twitch/raided.cjs
+++ src/events/twitch/raided.cjs
@@ -1,4 +1,4 @@
-module.exports = async (discordClient, _channel, username, viewers) => {
+module.exports = async (discordClient, _twitchClient, _channel, username, viewers) => {
   const discordChannel = discordClient.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
 
   if (discordChannel) {
