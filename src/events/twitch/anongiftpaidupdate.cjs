7171f8be9d646973ec8ef2b912c756a3385a15e7
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-08-12 19:42:56 +0200

    [ADD] src/events/twitch/anongiftpaidupdate.cjs

---
+++ src/events/twitch/anongiftpaidupdate.cjs
@@ -0,0 +1,17 @@
+module.exports = async (discordClient, channel, username, _userstate) => {
+  const discordChannel = discordClient.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
+
+  if (discordChannel) {
+    let subMessage = `### 🎉 A new subscriber! \`${username}\` just subscribed to \`${channel}\`!`;
+
+    discordChannel.send(subMessage)
+      .then(() => {
+        console.log(`Relayed subscription notification to Discord: "${subMessage}"`);
+      })
+      .catch(error => {
+        console.error('Error relaying subscription to Discord:', error);
+      })
+  } else {
+    console.error(`Error: Channel ${discordChannel} not found.`);
+  }
+}
