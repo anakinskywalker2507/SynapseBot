1433a255b58fcecec9281a2bf6883cc082bc8289
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-08-12 22:18:29 +0200

    [UPDATE] message.cjs
    [ADD] twitchClient parameter & commands handling

--- src/events/twitch/message.cjs
+++ src/events/twitch/message.cjs
@@ -1,4 +1,4 @@
-module.exports = async (discordClient, channel, tags, message, self) => {
+module.exports = async (discordClient, twitchClient, channel, tags, message, self) => {
   if (self) return;
   const discordChannel = discordClient.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
 
@@ -6,5 +6,24 @@ module.exports = async (discordClient, channel, tags, message, self) => {
     discordChannel.send(`\`${tags['display-name']}\` **(\`${channel}\`):** ${message}`);
   } else {
     console.error(`Error: Channel ${discordChannel} not found.`);
+  }
+  if (message.startsWith(twitchClient.prefix)) {
+    let messageArray = message.split(" ");
+    let commandName = messageArray[0].slice(twitchClient.prefix.length);
+    let args = messageArray.slice(1);
+
+    let command;
+    try {
+      command = require(`../../cmds/twitch/${commandName}.mjs`).command;
+      command.run(twitchClient, message, args);
+    } catch {
+      twitchClient.say(`Error: \"${commandName}\" No such command.`)
+        .then(() => {
+          console.log(`Relayed message from Discord to Twitch: "Error: \"${commandName}\" No such command."`);
+        })
+        .catch(error => {
+          console.error('Error relaying message to Twitch:', error);
+        })
+    }
   }
 }
