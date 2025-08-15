e35f013a2a912dc88126a280bc0d792f83e9d1f3
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-08-15 08:41:56 +0200

    [ADD] twitch/src/events/resub.cjs

---
+++ twitch/src/events/resub.cjs
@@ -0,0 +1,18 @@
+module.exports = async (_twitchClient, _channel, username, _months, _message, userstate) => {
+  let cumulativeMonths = ~~userstate["msg-param-cumulative-months"];
+  const discordChannel = discordClient.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
+
+  if (discordChannel) {
+    let subMessage = `### ✨ Re-sub from \`${username}\`! This is their ${cumulativeMonths} month in a row!`;
+
+    discordChannel.send(subMessage)
+      .then(() => {
+        console.log(`Relayed subscription notification to Discord: "${subMessage}"`);
+      })
+      .catch(error => {
+        console.error('Error relaying subscription to Discord:', error);
+      })
+  } else {
+    console.error(`Error: Channel ${discordChannel} not found.`);
+  }
+}
