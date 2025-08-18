0a713ac8961527cc4c6b8073a15a9bc89f28f8b6
YetAnotherMechanicusEnjoyer <ahmed.al-rawi@epitech.eu>
2025-08-18 03:01:52 +0200

    [UPDATE] connected.cjs
    [ADD] twitchClient.color()

--- twitch/src/events/connected.cjs
+++ twitch/src/events/connected.cjs
@@ -1,4 +1,10 @@
-module.exports = async (_twitchClient, address, port) => {
+module.exports = async (twitchClient, address, port) => {
   console.log(`[Twitch Bot] Connected to ${address}:${port}`);
+  twitchClient.color(twitchClient.configColor)
+    .then((data) => {
+      console.log(`Client color is set to ${data}`);
+    }).catch((err) => {
+      console.error(`Color Error: ${err}`);
+    });
 }
 
